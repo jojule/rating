27c49a7413d18e49d0e9dbffa1b0b0ca19017e9d
Joonas Lehtinen <joonas@vaadin.com>
2014-06-25 15:22:41 -0700

    Removed add-ons

--- src/main/java/org/vaadin/rating/ui/PresentationRating.java
+++ src/main/java/org/vaadin/rating/ui/PresentationRating.java
@@ -1,11 +1,14 @@
 package org.vaadin.rating.ui;
 
 import com.vaadin.data.Property;
+import com.vaadin.data.validator.IntegerRangeValidator;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
 import org.vaadin.rating.service.RatingService;
 import org.vaadin.rating.service.User;
 import org.vaadin.rating.service.data.Presentation;
-import org.vaadin.teemu.ratingstars.RatingStars;
 
 import javax.inject.Inject;
 
@@ -17,33 +20,42 @@ public class PresentationRating extends HorizontalLayout {
     @Inject
     User user;
 
-    RatingStars myRating = new RatingStars();
-    RatingStars avgRating = new RatingStars();
+    TextField myRating = new TextField("My rating");
+    TextField avgRating = new TextField("Average rating");
 
     PresentationRating() {
         addComponents(myRating, avgRating);
 
-        avgRating.setCaption("Average rating");
         avgRating.setReadOnly(true);
-        myRating.setCaption("My rating");
 
         myRating.addValueChangeListener(this::rate);
+        myRating.setNullSettingAllowed(true);
+        myRating.setNullRepresentation("-");
     }
 
     void rate(Property.ValueChangeEvent e) {
-        service.setRating(user.getEmail(), presentation, myRating.getValue());
-        updateAverage();
+        try {
+            if (myRating.getValue() != null) {
+                int myRatingValue = Integer.valueOf(myRating.getValue());
+                if (myRatingValue < 1 || myRatingValue > 5)
+                    Notification.show("Rating must be 1-5");
+                service.setRating(user.getEmail(), presentation, myRatingValue);
+                updateAverage();
+            }
+        } catch (NumberFormatException ignored) {
+        }
     }
 
     void updateAverage() {
         avgRating.setReadOnly(false);
-        avgRating.setValue(service.getRating(presentation));
+        avgRating.setValue(String.valueOf(service.getRating(presentation)));
         avgRating.setReadOnly(true);
     }
 
     void setPresentation(Presentation presentation) {
         this.presentation = presentation;
-        myRating.setValue(service.getRating(presentation, user.getEmail()));
+        int myRatingValue = (int) service.getRating(presentation, user.getEmail());
+        myRating.setValue(myRatingValue < 1 ? null : String.valueOf(myRatingValue));
         updateAverage();
     }
 }
